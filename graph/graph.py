5e65df1f02c6c219c357cc466ffd88827dfc347a
Huang Yixiao <huangyixiao@outlook.com>
2023-02-01 13:48:04 +0800

    Update graph.py
    Clear unused method

--- graph/graph.py
+++ graph/graph.py
@@ -22,16 +22,6 @@ class Graph(object):
     def get_neighborhood(self, node):
         return self.g.neighbors(node)
 
-    def get_connected_edges(self, node):
-        # TODO Special treatment due to the undirected graph nature.
-        #    The edges connected to node 4 might be (4, 2), (4, 6), (4, 8).
-        #    However, the edges we use to define variables are (2, 4), (4, 6), (4, 8),
-        #    where the first one is flipped.
-        # TODO Thinking it again, we do not have variables based on edges.
-        edges = self.g.edges(node)
-        edges = [_flip_edge(u, v) for (u, v) in edges]
-        return edges
-
     def get_nodes(self) -> list:
         return list(self.g.nodes)
 
@@ -61,8 +51,3 @@ class Graph(object):
 
     def max_clique(self):
         return graph_clique_number(self.g)
-
-
-def _flip_edge(u, v):
-    return (u, v) if u <= v else (v, u)
-
