86780e3e4a4110425328681648eac35ff35df268
Huang Yixiao <huangyixiao@outlook.com>
2023-02-09 21:01:36 +0800

    Refactor heuristics
    - Refactor heuristics for readability
    - Update the tests

--- model/heuristics.py
+++ model/heuristics.py
@@ -7,22 +7,20 @@ def random_coloring(graph: Graph) -> int:
     total_colors = 0
     color_dict = dict()
     for node in graph.get_nodes():
-        # node is not colored yet
-        if node not in color_dict:
-            # the first node encountered
-            if total_colors == 0:
-                color_dict[node] = total_colors
-                total_colors += 1
-            else:
-                neighbors = graph.get_neighborhood(node)
-                neighbor_colors = {color_dict[i] for i in neighbors if i in color_dict}
-                if len(neighbor_colors) == total_colors:
-                    color_dict[node] = total_colors
-                    total_colors += 1
-                else:
-                    set_colors = set(range(total_colors))
-                    random_available_color = set_colors.difference(neighbor_colors).pop()
-                    color_dict[node] = random_available_color
+        # only process node that is not colored yet
+        if node in color_dict:
+            continue
+        neighbors = graph.get_neighborhood(node)
+        neighbor_colors = {color_dict[i] for i in neighbors if i in color_dict}
+        if len(neighbor_colors) == total_colors:
+            # Case 1. Need a new color.
+            color_dict[node] = total_colors
+            total_colors += 1
+        else:
+            # Case 2. Use an existing color (randomly pop one).
+            set_colors = set(range(total_colors))
+            random_available_color = set_colors.difference(neighbor_colors).pop()
+            color_dict[node] = random_available_color
     return total_colors
 
 
@@ -65,7 +63,7 @@ def _node_filter(graph: Graph, next_node, node_degree: int, clique: list, clique
     """
     判断一个点是否可以加入完全子图集合中。
     情况1. 点的度小于完全子图的点数量，不可以加入（short-cut)
-    情况2. 点已经出现在完全子图中，不可以加入
+    情况2. 点已经出现在完全子图中，不可以加入（short-cut）
     情况3. 如果点和完全子图中每个点都相连，可以加入
     :param graph: 图
     :param next_node: 待判断点
--- test/model/test_heuristics.py
+++ test/model/test_heuristics.py
@@ -2,13 +2,21 @@ from unittest import TestCase
 
 from graph.graph import Graph
 from input_handler.reader import read_graph_from_file
-from model.heuristics import find_clique
+from model.heuristics import find_clique, random_coloring
 from util.util import add_input_cwd
 
 
 class Test(TestCase):
+    def setUp(self):
+        self.graph = read_graph_from_file(add_input_cwd('jean.col'))
+
     def test_find_large_clique(self):
-        graph = read_graph_from_file(add_input_cwd('jean.col'))
-        clique_size, clique = find_clique(graph)
+        clique_size, clique = find_clique(self.graph)
         print(f'Clique found with size {clique_size}: {clique}')
-        print(f'Max clique found with size {graph.max_clique()}')
+        print(f'Max clique found with size {self.graph.max_clique()}')
+        self.assertEqual(self.graph.max_clique(), 10)
+
+    def test_random_coloring(self):
+        num_color = random_coloring(self.graph)
+        print(f'Coloring is: {num_color}')
+        self.assertEqual(num_color, 10)
